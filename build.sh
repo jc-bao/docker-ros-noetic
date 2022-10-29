beac9449ba97f2f74f7b13b8e32d74afe1fc0a63
jc-bao <jc-bao@outlook.com>
2022-10-29 23:03:11 +0800

    add moveit

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
+if [ $# -gt 0 ]
+then
+    file=$1
+    target="ros_$1"
+else
+    file="docker_base"
+    target="nvidia-noetic"
+fi
+
 docker build --squash \
     --network host \
     --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy --build-arg all_proxy=$all_proxy \
-    -t nvidia-noetic -f docker_base
+    -t $target -f $file .
--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@ docker run -it --net=host --gpus all \
     --env="NVIDIA_DRIVER_CAPABILITIES=all" \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
+    --env="http_proxy=$http_proxy" \
+    --env="https_proxy=$https_proxy" \
+    --env="all_proxy=$all_proxy" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    nvidia-noetic \
+    --volume="${PWD}/ws_moveit:/ws_moveit" \
+    ros_docker_moveit \
     zsh
